d16073b732f6661198eb37e6405096284cf9478a
xiaoquqi <xiaoquqi@f96173b2-7949-0410-b239-bf3b9b5facb2>
2008-10-29 15:05:31 +0000

    Update open-flash-chart to latest version

---
+++ vendor/plugins/open_flash_chart/init.rb
@@ -0,0 +1,5 @@
+require 'open_flash_chart'
+
+ActionView::Base.send :include, OpenFlashChart::View
+ActionController::Base.send :include, OpenFlashChart::Controller
+ActionController::Base.send :include, OpenFlashChart
